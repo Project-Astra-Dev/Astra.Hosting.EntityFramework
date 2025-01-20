06a2167d9169b7293d771c2bf890c2bb1ede20c3
nexus <89035633+nexusverypro@users.noreply.github.com>
2025-01-20 18:45:48 +0000

    Changed project location path (breaking)

---
+++ Astra.Hosting.EntityFramework/Contexts/AstraDatabaseContextBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+namespace Astra.Hosting.EntityFramework.Contexts;
+
+public interface IAstraDatabaseContext
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    
+    string DatabaseName { get; }
+}
+
+public abstract class AstraDatabaseContextBase : DbContext, IAstraDatabaseContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite($"Data Source=astra_{DatabaseName.ToLower()}.db");
+
+    public abstract string DatabaseName { get; }
+}
